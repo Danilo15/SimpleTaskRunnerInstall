e9b727ec4b96f4939cd25e692d9190bbd1f9a994
Danilo15 <danilooliveira9@hotmail.com>
2015-08-04 14:24:40 -0300

    verificacao programas instalados

---
+++ TaskRunnerInstall/InstalledPrograms.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TaskRunnerInstall
+{
+    public static class InstalledPrograms
+    {
+        private static string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+        public static List<string> GetInstalledPrograms()
+        {
+            var result = new List<string>();
+            result.AddRange(GetInstalledProgramsFromRegistry(RegistryView.Registry32, RegistryHive.LocalMachine));
+            result.AddRange(GetInstalledProgramsFromRegistry(RegistryView.Registry64, RegistryHive.LocalMachine));
+
+            result.Sort();
+            result = result.Distinct().ToList();
+            return result;
+        }
+
+        private static IEnumerable<string> GetInstalledProgramsFromRegistry(RegistryView registryView, RegistryHive currentMachine)
+        {
+            var result = new List<string>();
+            try { 
+            using (RegistryKey key = RegistryKey.OpenBaseKey(currentMachine, registryView).OpenSubKey(registry_key))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        if (IsProgramVisible(subkey))
+                        {
+                            result.Add((string)subkey.GetValue("DisplayName"));
+                        }
+                    }
+                }
+            }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: {0}", e.Message);
+            }
+            return result;
+        }
+
+        private static bool IsProgramVisible(RegistryKey subkey)
+        {
+            var name = (string)subkey.GetValue("DisplayName");
+            var releaseType = (string)subkey.GetValue("ReleaseType");
+            //var unistallString = (string)subkey.GetValue("UninstallString");
+            var systemComponent = subkey.GetValue("SystemComponent");
+            var parentName = (string)subkey.GetValue("ParentDisplayName");
+
+            return
+                !string.IsNullOrEmpty(name)
+                && string.IsNullOrEmpty(releaseType)
+                && string.IsNullOrEmpty(parentName)
+                && (systemComponent == null);
+        }
+    }
+}
--- TaskRunnerInstall/Program.cs
+++ TaskRunnerInstall/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 
@@ -17,21 +18,30 @@ namespace TaskRunnerInstall
 
             folderToSave = string.Format(folderToSave, Environment.UserName);
             Download(WebConfigSettings.NodeJS);
-            Command(string.Format("cd {0} && msiexec /qb /i {1}", folderToSave, nodeJS), "Instalando NodeJS");
+            Instalar(string.Format("cd {0} && msiexec /qb /i {1}", folderToSave, nodeJS), "Node.js");
             Command("npm update -g npm", "Atualizando NPM");
             Command("npm install -g grunt-cli grunt gulp", "Instalando grunt-cli, grunt e gulp");
             Download(WebConfigSettings.GruntLauncher);
-            Command(string.Format("\"{1}\" /q {0}/{2}", folderToSave, WebConfigSettings.VSIXInstaller, gruntLauncher), "Instalando Grunt Launcher");
+            Instalar(string.Format("\"{1}\" /q {0}/{2}", folderToSave, WebConfigSettings.VSIXInstaller, gruntLauncher), "Grunt Launcher");
             Download(WebConfigSettings.TaskRunnerExplorer);
-            Command(string.Format("\"{1}\" /q {0}/{2}", folderToSave, WebConfigSettings.VSIXInstaller, taskRunnerExplorer), "Instalando Task Runner Explorer");
+            Instalar(string.Format("\"{1}\" /q {0}/{2}", folderToSave, WebConfigSettings.VSIXInstaller, taskRunnerExplorer), "Task Runner Explorer");
             Download(WebConfigSettings.RubyGems);
-            Command(string.Format("{0}/{1} /VERYSILENT", folderToSave, rubyGems), "Instalando RubyGems");
+            Instalar(string.Format("{0}/{1} /VERYSILENT", folderToSave, rubyGems), "Ruby 2.2.2-p95-x64");
             Command(@"set PATH=%PATH%;C:\Ruby22-x64\bin;", "Adicionando RubyGems nas Variaveis de Ambiente");
-            Command("gem install sass thor sasslint", "Instalando SASS, Thor e SASSLINT");
-            Console.WriteLine("Digite o path do projeto Web");
+            Command("gem install sass scss_lint", "Instalando SASS, SCSS_LINT");
+            Console.WriteLine("Digite o path do projeto Web ou da pasta que contém o package.json");
             Command(string.Format("cd {0} && npm install && npm install -g", Console.ReadLine()), "Instalando packages no projeto");
             Console.WriteLine("Abra o Visual Studio 2013 vá em View > Other Windows > Task Runner Explorer\nClique no botao de atualizar e rode a task dev ou dev-build");
             Console.ReadKey();
+        }
+
+        private static void Instalar(string command, string description)
+        {
+            if (!GetInstalledPrograms(description))
+            {
+                Console.WriteLine("Instalando {0}", description);
+                Command(command, description);
+            }
         }
 
         private static void Command(string command, string description)
@@ -53,12 +63,35 @@ namespace TaskRunnerInstall
             string nameToSave = folderToSave + "/" + splitedLink[splitedLink.Length - 1];
             string nameVerify = string.Format("{0}", nameToSave.Split('/')[nameToSave.Split('/').Length - 1]);
             var containsFile = Directory.GetFiles(folderToSave, nameVerify, SearchOption.AllDirectories);
-            if(containsFile == null){
+            if (containsFile == null)
+            {
                 Console.WriteLine("Baixando {0}...", splitedLink[splitedLink.Length - 1]);
                 webClient.DownloadFile(new Uri(linkDownload), nameToSave);
-            }else{
+            }
+            else
+            {
                 Console.WriteLine("Já contém {0}...", splitedLink[splitedLink.Length - 1]);
             }
+        }
+
+        public static bool GetInstalledPrograms(string installedItem)
+        {
+            List<string> programas = InstalledPrograms.GetInstalledPrograms();
+            var id = programas.BinarySearch(installedItem);
+            string t = string.Empty;
+            try
+            {
+                 t = programas[id];
+            }catch(Exception e){
+                return false;
+            }
+            
+            if (t == installedItem)
+            {
+                Console.WriteLine("{0} já está instalado", t);
+                return true;
+            }
+            return false;
         }
     }
 }
